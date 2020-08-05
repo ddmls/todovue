2221a4c047b7254795e0326e3efece28672563ec
Dimos Dimoulis <58330181+ddmls@users.noreply.github.com>
2020-08-05 18:40:00 +0300

    Animation for title and subtitle
    Use light color for title and subtitle

--- src/components/DaySummary.vue
+++ src/components/DaySummary.vue
@@ -3,8 +3,8 @@
   <header class="hero">
   <!-- <div class="container is-fluid"> -->
     <div class="hero-body">
-      <h1 class="title is-2 is-family-secondary"> {{ msg }} </h1>
-      <h2 class="subtitle is-4"> {{ dateTimeFormat.format(new Date()) }} </h2>
+      <h1 class="title is-2 is-family-secondary has-text-light"> {{ msg }} </h1>
+      <h2 class="subtitle is-4 has-text-light"> {{ dateTimeFormat.format(new Date()) }} </h2>
     </div>
   <!-- </div> -->
   </header>
@@ -24,6 +24,50 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.title {
+  letter-spacing: .2rem;
+  animation-name: spaceout-fadein;
+  animation-duration: 2s;
+  animation-timing-function: ease;
+  // animation: 2s ease-out spaceout;
+}
+
+.subtitle {
+    animation-name: delayed-fadein;
+    animation-duration: 2s;
+    // animation-delay: 1s;
+    animation-timing-function: ease;
+}
+
+// TODO: must make sure image has been loaded
+// e.g. event "load"
+
+@keyframes spaceout-fadein {
+  from {
+    letter-spacing: inherit;
+    opacity: 0;
+  }
+
+  to {
+    letter-spacing: .2rem;
+    opacity: 1;
+  }
+}
+
+@keyframes delayed-fadein {
+  from {
+    opacity: 0;
+  }
+
+  50% {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+
 header {
   background-image: url('../assets/DSC_0195_v1.jpg');
   background-position: center;
