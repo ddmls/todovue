bb60f79872cf6416a7b5e54b009f653f5f145165
Dimos Dimoulis <58330181+ddmls@users.noreply.github.com>
2020-08-06 15:09:36 +0300

    Start of bulma customization

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import 'bulma/css/bulma.css'
+// import 'bulma/css/bulma.css'
+import './bulma-custom.scss'
 // import '@fortawesome/fontawesome-free/js/all'
 import '@fortawesome/fontawesome-free/css/all.css'
 
